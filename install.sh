9857181f36efe61ad87a7e6ac2cdd729f4b8e691
G Man <genx7up@users.noreply.github.com>
2017-03-30 17:16:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,20 +31,4 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     yum -y install neovim jq
 fi
 
-echo "creating vim directories"
-mkdir -p ~/.vim-tmp
-
-if ! command_exists zsh; then
-    echo "zsh not found. Please install and then re-run installation scripts"
-    exit 1
-elif ! [[ $SHELL =~ .*zsh.* ]]; then
-    echo "Configuring zsh as default shell"
-    chsh -s $(which zsh)
-fi
-
-if ! command_exists zplug; then
-    echo "installing zplug, a plugin manager for zsh - http://zplug.sh"
-    git clone https://github.com/zplug/zplug ~/.zplug
-fi
-
 echo "Done. Reload your terminal."
