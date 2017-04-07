53636631add6bb45b93dab8b5f01a48e3a8d276b
G Man <genx7up@users.noreply.github.com>
2017-04-07 13:09:59 -0700

    Update install.sh

--- tmux/install.sh
+++ tmux/install.sh
@@ -21,6 +21,10 @@ make
 sudo make install
 cd ..
 
+#install tpm plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
 #cleanup
 rm -rf libevent*
 rm -rf tmux-2.3*
