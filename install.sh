907a473bf609877d0707228c7c0282b43dacc8a1
G Man <genx7up@users.noreply.github.com>
2017-04-09 13:53:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ command_exists() {
     type "$1" > /dev/null 2>&1
 }
 
-echo "Creating/Syncing IDE environment ..."
-read -t 10 -p "Running 'idesync' in 10s. Hit ENTER to begin now or ^C to abort";
-
 echo "Installing dotfiles."
 
 echo "Initializing submodule(s)"
