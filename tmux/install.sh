0a68733e722e6e9c2707fcdc57b68517a1da4709
G Man <genx7up@users.noreply.github.com>
2017-04-08 21:39:34 -0700

    Update install.sh

--- tmux/install.sh
+++ tmux/install.sh
@@ -29,3 +29,13 @@ mv fzf /usr/local/bin/
 rm -rf libevent*
 rm -rf tmux-2.3*
 rm -rf fzf-0.16*
+
+#install plugins
+# start a server but don't attach to it
+tmux start-server
+# create a new session but don't attach to it either
+tmux new-session -d
+# install the plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# killing the server is not required, I guess
+tmux kill-server
