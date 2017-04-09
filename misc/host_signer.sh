de205f0a4c4bc2f48b9dbd9b93908ba781fbc27e
G Man <genx7up@users.noreply.github.com>
2017-04-08 20:13:29 -0700

    Update host_signer.sh

--- misc/host_signer.sh
+++ misc/host_signer.sh
@@ -89,7 +89,7 @@ awk 'NR==FNR { if ($1 ~ /^UID_(MIN|MAX)$/) m[$1] = $2; next }
 done
 
 # Client setup
-curl -sSL https://goo.gl/iu5a1N -o /usr/local/bin/volt.sh
+curl -sSL https://raw.githubusercontent.com/genx7up/dotfiles/master/misc/volt.sh -o /usr/local/bin/volt.sh
 echo 'function volt() { bash /usr/local/bin/volt.sh $@ && source /usr/local/bin/volt.sh load; }' >> /etc/profile
 echo 'if [[ -f /usr/local/bin/volt.sh ]]; then source /usr/local/bin/volt.sh load; fi' >> /etc/profile
 
