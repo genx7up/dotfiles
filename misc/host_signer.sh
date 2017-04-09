1e9cfc60aca41b92184f9fbc72cd5d3fe9681d0e
G Man <genx7up@users.noreply.github.com>
2017-04-08 20:55:48 -0700

    Update host_signer.sh

--- misc/host_signer.sh
+++ misc/host_signer.sh
@@ -93,4 +93,6 @@ curl -sSL https://raw.githubusercontent.com/genx7up/dotfiles/master/misc/volt.sh
 echo 'function volt() { bash /usr/local/bin/volt.sh $@ && source /usr/local/bin/volt.sh load; }' >> /etc/profile
 echo 'if [[ -f /usr/local/bin/volt.sh ]]; then source /usr/local/bin/volt.sh load; fi' >> /etc/profile
 
-
+# IDE bootstrap
+echo "bash <(curl -sSL https://raw.githubusercontent.com/genx7up/dotfiles/master/bootstrap.sh)" > /usr/local/bin/idesync
+chmod +x /usr/local/bin/idesync
