71232ff9c5157e8d8390e87133f9c35172d893f4
G Man <genx7up@users.noreply.github.com>
2017-04-09 13:53:21 -0700

    Update client_setup.sh

--- misc/client_setup.sh
+++ misc/client_setup.sh
@@ -9,7 +9,7 @@ fi
 curl -sSL https://raw.githubusercontent.com/genx7up/dotfiles/master/misc/volt.sh -o /usr/local/bin/volt.sh
 echo 'function volt() { bash /usr/local/bin/volt.sh $@ && source /usr/local/bin/volt.sh load; }' >> /etc/profile
 echo 'if [[ -f /usr/local/bin/volt.sh ]]; then source /usr/local/bin/volt.sh load; fi' >> /etc/profile
-echo 'if [[ ! -d ~/.dotfiles ]]; then idesync; fi' >> /etc/profile
+echo 'if [[ ! -d ~/.dotfiles ]]; then /usr/local/bin/idesync; fi' >> /etc/profile
 
 # IDE bootstrap
 echo "bash <(curl -sSL https://raw.githubusercontent.com/genx7up/dotfiles/master/bootstrap.sh)" > /usr/local/bin/idesync
