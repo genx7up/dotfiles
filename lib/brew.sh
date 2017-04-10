9631ba45a766cade6d3844c0071595106f340f08
G Man <genx7up@users.noreply.github.com>
2017-04-09 17:20:41 -0700

    Update brew.sh

--- lib/brew.sh
+++ lib/brew.sh
@@ -18,23 +18,17 @@ brew upgrade
 # Add some casks
 brew tap caskroom/cask
 brew tap homebrew/dupes
+brew tap homebrew/versions
 brew services list
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
@@ -44,61 +38,52 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   chsh -s /usr/local/bin/bash;
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install more recent versions of some macOS tools.
-# brew install homebrew/dupes/grep
-# brew install homebrew/dupes/openssh
-
 # Install font tools.
 brew tap bramstein/webfonttools
 brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew install vbindiff
-brew install webkit2png
-brew install zopfli
-
 formulas=(
-    # flags should pass through the the `brew list check`
-    'macvim --with-override-system-vim'
+    ack
+    dark-mode
+    findutils
     fzf
+    git
+    git-lfs
     gpg
     gpg-agent
+    'gnu-sed --with-default-names'
     'grep --with-default-names'
     highlight
     hub
     httpie
+    'imagemagick --with-webp'
     jq
-    keybase
+    lua
+    'macvim --with-override-system-vim'
+    moreutils
     nano
     neovim/neovim/neovim
+    p7zip
+    pigz
+    pv
     reattach-to-user-namespace
+    rename
     ripgrep
+    rhino
+    speedtest_cli
+    ssh-copy-id
+    testssl
     the_silver_searcher
     tmux
+    tree
     unison
+    vbindiff
+    webkit2png
+    'wget --with-iri'
     z
+    zopfli
 )
 
 for formula in "${formulas[@]}"; do
@@ -116,7 +101,9 @@ gem install wbench
 brew tap caskroom/fonts   
 brew cask install font-firacode-nerd-font-mono
 brew cask install font-droidsansmono-nerd-font-mono
- 
+
+brew cask install keybase
+
 # Cleanup
 brew cleanup
 rm -f -r /Library/Caches/Homebrew/*
