435cb37e58075a6c64841ca2611417612af2ac35
genx7up <genx7up@local>
2018-08-17 03:43:56 +0000

    Fixing git username issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,12 +12,12 @@ SRC_DIR=~/src
 mkdir -p "$SRC_DIR"
 
 if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform        
+    # Do something under Mac OS X platform
     xcode-select --install && echo $?
 
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
-    
+
     ###### Install latest git from Rackspace repo
     RELEASEVER=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
     BASEARCH=$(uname -m)
@@ -30,19 +30,19 @@ sudo rm -rf /usr/local/bin/volt.sh
 sudo curl -sSL https://raw.githubusercontent.com/genx7up/dotfiles/master/misc/volt.sh -o /usr/local/bin/volt.sh
 bash /usr/local/bin/volt.sh login
 
-if [[ ! -f ~/.ssh/git_rsa.pub ]]; then 
+if [[ ! -f ~/.ssh/git_rsa.pub ]]; then
 	mkdir -p ~/.ssh && chmod 700 ~/.ssh
 	bash /usr/local/bin/volt.sh get git_rsa.pub > ~/.ssh/git_rsa.pub
 	bash /usr/local/bin/volt.sh get git_rsa > ~/.ssh/git_rsa
 	chmod 600 ~/.ssh/git_rsa
-	
+
 	if [[ $? == 0 && `cat ~/.ssh/git_rsa.pub` == "Not found" ]]; then
 		rm -rf ~/.ssh/git_rsa*
 		echo 'No git keys found. Do you want to generate & add new keys? (y/n)'
 		read choice < /dev/tty
 		if [[ $choice == 'y' ]]; then
 			ssh-keygen -t rsa -b 4096 -f ~/.ssh/git_rsa -q -N ""
-			if [[ -f ~/.ssh/git_rsa.pub ]]; then 
+			if [[ -f ~/.ssh/git_rsa.pub ]]; then
 				echo 'Copy/Paste the following public key to your git profile.'
 				cat ~/.ssh/git_rsa.pub
 				echo 'Reusability - Save new keys to remote vault? (y/n)'
@@ -51,7 +51,7 @@ if [[ ! -f ~/.ssh/git_rsa.pub ]]; then
 					bash /usr/local/bin/volt.sh set git_rsa.pub @$HOME/.ssh/git_rsa.pub
 					bash /usr/local/bin/volt.sh set git_rsa @$HOME/.ssh/git_rsa
 				fi
-			fi	
+			fi
 		fi
 	fi
 fi
@@ -67,13 +67,13 @@ cd ~/.dotfiles && git pull
 ./install.sh || echo $?
 
 # Customize git
-RES=$(git config --global user.name | wc -l)
+RES=$(git config user.name | wc -l)
 if [[ "$RES" == "0" ]]; then
 	printf "\n### Enter user.name for Git Commits\n"
 	read name < /dev/tty
 	set -x
-	git config --global user.name $name
-	git config --global user.email "$name@users.noreply.github.com"
+	git config user.name $name
+	git config user.email "$name@users.noreply.github.com"
 	set +x
 fi
 
