75b13f82e4f3938035c8fb5768909135bcff8eb9
G Man <genx7up@users.noreply.github.com>
2021-10-12 11:26:31 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
 
     ###### Install latest git from Rackspace repo
-    sudo yum -y install https://repo.ius.io/ius-release-el7.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    sudo yum -y install https://repo.ius.io/ius-release-el7.rpm || :
+    sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm || :
     sudo yum -y install git222
 fi
 
