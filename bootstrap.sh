a7efa8c0610890b6a20047dad7bd87a93f89727c
G Man <genx7up@users.noreply.github.com>
2022-12-09 10:39:10 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,10 +18,14 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
 
-    ###### Install latest git from Rackspace repo
-    sudo yum -y install https://repo.ius.io/ius-release-el7.rpm || :
-    sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm || :
-    sudo yum -y install git236
+    if [ "$(rpm --eval '%{centos_ver}')" == "7" ]; then
+        ###### Install latest git from Rackspace repo
+        sudo yum -y install https://repo.ius.io/ius-release-el7.rpm || :
+        sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm || :
+        sudo yum -y install git236
+    else
+        sudo yum -y install git
+    fi
 fi
 
 #get personal git keys
