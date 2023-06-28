8a5339e03a44264ac7848a8d3900a55079f16c8b
genx7up <genx7up@users.noreply.github.com>
2023-06-28 19:34:57 +0000

    Kubes alias

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@ EOF
     # Install minikube
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
     sudo rpm -Uvh minikube-latest.x86_64.rpm
-
+    minikube start --force
 
 fi
 
